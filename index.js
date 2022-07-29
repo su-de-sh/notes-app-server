8021762f1dfaaaa31a16c994921fe80014e35071
su-de-sh <sudesh7443@gmail.com>
2022-07-29 15:39:19 +0545

    feat: access data from database instead hardcode

--- index.js
+++ index.js
@@ -19,32 +19,34 @@ const requestLogger = (request, response, next) => {
 
 App.use(requestLogger);
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-1-17T17:30:31.098Z",
-    important: false,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    date: "2022-1-17T18:39:34.091Z",
-    important: true,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-1-17T19:20:14.298Z",
-    important: false,
-  },
-];
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     date: "2022-1-17T17:30:31.098Z",
+//     important: false,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only JavaScript",
+//     date: "2022-1-17T18:39:34.091Z",
+//     important: true,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     date: "2022-1-17T19:20:14.298Z",
+//     important: false,
+//   },
+// ];
 
 App.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 App.get("/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 App.get("/notes/:id", (request, response) => {
